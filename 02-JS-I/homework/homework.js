f77d3037311472957f20c5662a10fc33ed53bd3a
Cliford Torrez <torrescli26@gmail.com>
2022-01-16 13:13:55 -0600

    actualizaciónhomework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,6 +1,6 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-const { esVerdadero } = require("../../03-JS-II/homework/homework");
+const { esVerdadero, saludo } = require("../../03-JS-II/homework/homework");
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString ='HOLA';
@@ -159,20 +159,27 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-
-  
+  resta = num % 1;
+  entero = num - resta;
+  if (resta-0.5){
+    return  (entero+1);
+      }
+      else{
+        return(entero)
+      }
+  return matchMedia.round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+return Math.random()
 }
 
 function esPositivo(numero) {
@@ -180,19 +187,30 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero===0){
+    return("Falso");
+  }
+  if (numero>0){
+    return ("Es positivo");
+  }
+  else {
+    return ("Es negativo");
+  }
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  combinado = nombre + ' ' + apellido ;
+  return combinado ;
   
 }
 
@@ -200,13 +218,16 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  saludo = 'Hola'+ ' ' + nombre + '!';
+  return saludo;
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return area= (alto*ancho);
+
 }
 
 
