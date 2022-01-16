b0c6a85819709409ea0045312a3833feffc3f228
Cliford Torrez <torrescli26@gmail.com>
2022-01-16 09:46:40 -0600

    homework2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,16 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  x=8, y=10;
+  suma=x+y;
+  return suma;
 }
 
 function resta(x, y) {
